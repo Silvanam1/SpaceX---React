b7c0606821705b4f7251d7b4bca38d7264472f41
Silvanam1 <silvanabmaldonado@hotmail.com>
2025-03-10 19:42:27 -0300

    Pimera version de Lista de Lanzamientos con React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+export function App() {
+
+  return <div>Hola Mundo </div>;
+  
+}
+
+
